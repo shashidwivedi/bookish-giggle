b3f730e4b695916f1bcc3a8e8f39931ce1fe8ad4
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-03-05 18:04:59 +0530

    creating the scene - add the cube

--- main.js
+++ main.js
@@ -11,3 +11,10 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
