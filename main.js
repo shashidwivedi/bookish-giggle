d88147a03dcd7d9f98cc244c897ce6c7edd355ed
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-03-05 18:08:03 +0530

    rendering the scene

--- main.js
+++ main.js
@@ -18,3 +18,10 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
